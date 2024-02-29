74a8a5e8b1dcb536e68330a9774e410944e73d6e
CoderJourney-code <138033988+CoderJourney-code@users.noreply.github.com>
2024-02-28 17:29:26 -0800

    Adding lunch break capability

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 // import 'https://tomashubelbauer.github.io/github-pages-local-storage/index.js';
+// Created by Tyler Strasser
 
 function print(pass) { 
     console.log(pass);
 }
 
 let clock_in_data = new Date();
+let clock_out_time_data = new Date(); 
+let break_start_time_data = new Date();
+let break_end_time_data = new Date();
+let lunch_start_time_data = new Date();
+let lunch_end_time_data = new Date();
+
 let clocked_in = 0;
 let clocked_out = 0;
 let on_break = 0;
+let on_lunch = 0;
+
 let settings_data = {
     clocked_in: 0,
     clocked_out: 0,
     on_break : 0,
+    on_lunch: 0,
+    lunch_start_time: 0,
+    lunch_end_time: 0,
     break_start_time: 0,
     break_end_time: 0,
     clock_in_time: 0,
@@ -32,15 +44,18 @@ let myObject = JSON.parse(localStorage.getItem("myObject"));
 // localStorage.setItem("clock_in_data", clock_in_data);
 // var local_time = clock_in_data.toLocaleTimeString();
 
-let clock_out_time_data = new Date(); 
+
 
 function updateUI() {
-    clock_in_button.disabled = clocked_in || on_break;
-    clock_out_button.disabled = !clocked_in || on_break;
-    take_break_button.disabled = !clocked_in || on_break;
-    end_break_button.disabled = !on_break || !clocked_in;
+    clock_in_button.disabled = clocked_in || on_break || on_lunch;
+    clock_out_button.disabled = !clocked_in || on_break || on_lunch;
+    take_break_button.disabled = !clocked_in || on_break || on_lunch;
+    end_break_button.disabled = !on_break || !clocked_in || on_lunch;
+    take_lunch_button.disabled = !clocked_in || on_lunch || on_break;
+    end_lunch_button.disabled = !on_lunch || !clocked_in || on_break;
 
-    if (clocked_in && !on_break) {
+
+    if (clocked_in && !on_break && !on_lunch) {
         status_element.hidden = false;
         status_element.textContent = "Clocked in";
         status_element.style.color = 'rgb(0, 255, 38)';
@@ -48,7 +63,14 @@ function updateUI() {
         status_element.hidden = false;
         status_element.textContent = "On break";
         status_element.style.color = "red";
-    } else {
+    }
+    else if (on_lunch) {
+        status_element.hidden = false;
+        status_element.textContent = "On lunch";
+        status_element.style.color = "orange";
+    
+    } 
+    else {
         status_element.hidden = true;
     }
 
@@ -68,8 +90,11 @@ function loadSavedState() {
         clocked_in = data.clocked_in;
         clocked_out = data.clocked_out;
         on_break = data.on_break;
+        on_lunch = data.on_lunch;
         break_start_time_data = new Date(data.break_start_time);
         break_end_time_data = new Date(data.break_end_time);
+        lunch_start_time_data = new Date(data.lunch_start_time);
+        lunch_end_time_data = new Date(data.lunch_end_time);
         clock_in_data = new Date(data.clock_in_time);
         clock_out_time_data = new Date(data.clock_out_time);
         updateUI();
@@ -93,16 +118,19 @@ let clock_in_button = document.getElementById("clock_in");
 let clock_out_button = document.getElementById("clock_out");
 let take_break_button = document.getElementById("take_break");
 let end_break_button = document.getElementById("end_break");
+let take_lunch_button = document.getElementById("take_lunch");
+let end_lunch_button = document.getElementById("end_lunch");
 let clock = document.getElementById("clock");
 let status_element = document.getElementById("status");
 
 clock_in_button.addEventListener("click", clock_in);
 clock_out_button.addEventListener("click", clock_out);
+take_lunch_button.addEventListener("click", take_lunch);
+end_lunch_button.addEventListener("click", end_lunch);
 take_break_button.addEventListener("click", take_break);
 end_break_button.addEventListener("click", end_break);
 
-let break_start_time_data = new Date();
-let break_end_time_data = new Date();
+
 
 // localStorage.clear();
 loadSavedState();
@@ -110,8 +138,8 @@ loadSavedState();
 updateStorage();
 function clock_in() {
     clock_in_data = new Date();
-    clocked_in = 1;
     clock_out_time_data = new Date();
+    clocked_in = 1;
     clock_out_time_data.setHours(clock_in_data.getHours() + 8);
     updateUI();
     updateStorage();
@@ -126,6 +154,23 @@ function clock_out() {
 
 }
 
+function take_lunch() {
+    lunch_start_time_data = new Date();
+    on_lunch = 1;
+    updateUI();
+    updateStorage();
+
+}
+
+function end_lunch() {
+    lunch_end_time_data = new Date();
+    on_lunch = 0;
+    calculateBreakTime(lunch_start_time_data, lunch_end_time_data, regular_break=0);
+    updateUI();
+    updateStorage();
+
+}
+
 function take_break() {
     break_start_time_data = new Date();
     on_break = 1;
@@ -133,21 +178,27 @@ function take_break() {
     updateStorage();
 }
 
+
 function end_break() { 
     break_end_time_data = new Date();
     on_break = 0;
-    const diff_in_mins = calculateBreakTime(break_start_time_data, break_end_time_data);
-    const diff_in_hours = Math.floor(diff_in_mins / 60);
-    const remaining_mins = diff_in_mins % 60;
-    clock_out_time_data.setHours(clock_out_time_data.getHours() + diff_in_hours);
-    clock_out_time_data.setMinutes(clock_out_time_data.getMinutes() + remaining_mins);
+    calculateBreakTime(break_start_time_data, break_end_time_data);
     updateUI();
     updateStorage();
 }
 
-function calculateBreakTime(start, end) {
-    const diffInMinutes = Math.abs(end - start) / 60000;
-    return diffInMinutes;
+function calculateBreakTime(start, end, regular_break=1) {
+    let diff_test = end - start;
+    let diff_in_mins = Math.abs(end - start) / 60000;
+    
+    if (regular_break == 1) {
+        diff_in_mins = Math.max(diff_in_mins - 10, 0);
+    }
+    const diff_in_hours = Math.floor(diff_in_mins / 60);
+    const remaining_mins = diff_in_mins % 60;
+    clock_out_time_data.setHours(clock_out_time_data.getHours() + diff_in_hours);
+    clock_out_time_data.setMinutes(clock_out_time_data.getMinutes() + remaining_mins);
+    return diff_in_mins;
 }
 
 
